92943ea5cc43b945fd34f5d680cb8d7930ceec1e
James McCarthy <hikerbikerwriter@gmail.com>
2018-12-02 09:45:40 -0800

    updated db config fie and fixed bug on footer

--- view/snip_footer.php
+++ view/snip_footer.php
@@ -8,6 +8,7 @@
  * @Last modified time: 05-18-2017 7:08:04
  * @Copyright: 2017
  */
+?>
 
 <div class="footer-copy">
 	<p style="margin-top: 20px; font-size: .8em; text-align: center">&copy; 2017  jMcCarthy / v 1.01 BETA</p>
